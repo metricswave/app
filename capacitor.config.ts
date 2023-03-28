fd7a8e4c7924657f31e5e84bc7f441a85d389d10
Víctor Falcón <victoor89@gmail.com>
2023-03-28 10:35:48 +0200

    Add iOS and Android app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.notifywave.app',
+  appName: 'NotifyWave',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
