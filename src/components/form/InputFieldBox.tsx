7e9bc1e8e455a20ae7ebc6ebede4a3ceccb8226a
Víctor Falcón <victoor89@gmail.com>
2023-03-29 15:57:05 +0200

    Small style fixes

--- src/components/form/InputFieldBox.tsx
+++ src/components/form/InputFieldBox.tsx
@@ -13,12 +13,12 @@ export default function InputFieldBox({value, setValue, label, name, type = "tex
     return (
             <div className="flex flex-col border transition-all border-zinc-200/60 hover:border-zinc-200 focus-within:hover:border-zinc-300 focus-within:border-zinc-300 duration-300 dark:border-zinc-700/60 rounded-sm hover:dark:border-zinc-700 group focus-within:dark:border-zinc-600 hover:focus-within:dark:border-zinc-600">
 
-                <label className="text-xs pt-4 px-4 pb-1 transition-all group-focus-within::opacity-100 duration-300 opacity-70 group-hover:opacity-90 group-focus-within:opacity-100 hover:group-focus-within:opacity-100"
+                <label className="text-xs pt-4 px-4 pb-1 transition-all group-focus-within::opacity-100 duration-300 opacity-50 dark:opacity-60 group-hover:opacity-80 group-focus-within:opacity-100 hover:group-focus-within:opacity-100"
                        htmlFor={name}>
                     {label}
                 </label>
 
-                <input className="pt-1 px-4 pb-4 bg-transparent outline-none"
+                <input className="pt-1 px-4 pb-4 bg-transparent outline-none placeholder:opacity-70"
                        value={value}
                        onChange={e => setValue(e.target.value)}
                        type={type}
