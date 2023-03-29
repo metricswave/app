4a4527000c4ad6508c3c1bd0ee0761c5801d3d01
Víctor Falcón <victoor89@gmail.com>
2023-03-29 16:13:38 +0200

    Create a config json file

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,8 +1,10 @@
+import {app} from "../../config/app"
+
 export default function Logo({className, ...props}: { className?: string }) {
     return (
             <a className={`${className} flex items-center space-x-4 text-zinc-900 dark:text-white`} href="/">
                 <div className="inline-block rounded-sm h-6 w-6 bg-gradient-to-b from-pink-500 to-amber-500"></div>
-                <span className="font-bold tracking-tighter">NotifyWave</span>
+                <span className="font-bold tracking-tighter">{app.name}</span>
             </a>
     )
 }
--- src/components/wrappers/Authentication.tsx
+++ src/components/wrappers/Authentication.tsx
@@ -3,8 +3,8 @@ import Logo from "../logo/Logo"
 
 export default function Authentication({children, footer}: { children: React.ReactNode, footer?: React.ReactNode }) {
     return (
-            <div className="w-full h-screen flex flex-col items-center justify-center p-4">
-                <div className="bg-white dark:bg-zinc-800 rounded-sm shadow-sm p-10 mx-10 max-w-full w-[416px]">
+            <div className="w-full h-screen flex bg-white sm:bg-transparent flex-col items-center justify-center p-4">
+                <div className="bg-white dark:bg-zinc-800 rounded-sm sm:shadow-sm p-10 mx-10 max-w-full w-[416px]">
                     <Logo className="mb-10"/>
 
                     {children}
