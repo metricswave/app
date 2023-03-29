a629613590bc14439d64de9c8f7b9a3b894dbc0c
Víctor Falcón <victoor89@gmail.com>
2023-03-30 00:02:58 +0200

    Make functional login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,11 +2,76 @@ import InputFieldBox from "../components/form/InputFieldBox"
 import PrimaryButton from "../components/form/PrimaryButton"
 import Authentication from "../components/wrappers/Authentication"
 import LinkButton from "../components/buttons/LinkButton"
-import {useState} from "react"
+import {FormEvent, useState} from "react"
+import {fetchApi} from "../helpers/ApiFetcher"
+import {Tokens} from "../types/Token"
+import {DeviceName} from "../storage/DeviceName"
 
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const [formError, setFormError] = useState<string | false>(false)
+    const [errors, setErrors] = useState<{ email: false | string, password: false | string }>({
+        email: false,
+        password: false,
+    })
+
+    const isValid = (): boolean => {
+        let hasErrors = false
+        let e = {}
+
+        if (email === "") {
+            e = {...e, email: "Email is required"}
+        } else if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) {
+            hasErrors = true
+            e = {...e, email: "Invalid email"}
+        } else {
+            e = {...e, email: false}
+        }
+
+        if (password === "") {
+            hasErrors = true
+            e = {...e, password: "Passwords is required"}
+        } else {
+            e = {...e, password: false}
+        }
+
+        setErrors({...errors, ...e})
+
+        return !hasErrors
+    }
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault()
+
+        if (!isValid()) return
+
+        setLoading(true)
+        setFormError(false)
+
+        fetchApi<Tokens>("/login", {
+            method: "POST",
+            body: {
+                email,
+                password,
+                device_name: DeviceName.name(),
+            },
+            success: (data) => {
+                localStorage.setItem("nw:auth", JSON.stringify(data.data))
+                window.location.href = "/"
+                setLoading(false)
+            },
+            error: (data) => {
+                setFormError(data.message)
+                setLoading(false)
+            },
+            catcher: (data) => {
+                setLoading(false)
+            },
+        })
+
+    }
 
     return (
             <Authentication footer={
@@ -14,7 +79,7 @@ export default function Login() {
                     Do not have an account? <LinkButton href="/auth/signup" text="Sign Up →"/>
                 </p>
             }>
-                <form className="mt-8">
+                <form onSubmit={handleSubmit} className="mt-8">
                     <div className="flex flex-col space-y-4">
                         <InputFieldBox value={email}
                                        setValue={setEmail}
@@ -30,7 +95,9 @@ export default function Login() {
                                        placeholder="Password"
                                        type="password"/>
 
-                        <PrimaryButton text="Log In"/>
+                        {formError && <p className="text-red-500 text-sm py-2 text-center">{formError}</p>}
+
+                        <PrimaryButton text="Log In" loading={loading}/>
                     </div>
                 </form>
             </Authentication>
