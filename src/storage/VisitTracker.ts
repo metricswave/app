0340204ab2289f11f144bdc91d35ac9e3d5d045e
Víctor Falcón <victoor89@gmail.com>
2023-06-08 13:50:57 +0200

    Change URL to track visits in localhost

--- src/storage/VisitTracker.ts
+++ src/storage/VisitTracker.ts
@@ -6,12 +6,14 @@ export const TrackVisit = function () {
     const location = useLocation()
 
     useEffect(() => {
+        let path = "https://notifywave.com/webhooks/f41ff0fd-4475-499c-b086-82d6012bbf16"
+
         if (!app.isProduction) {
             console.log("VisitTracker", location.pathname)
-            return
+            path = "http://notifywave.test/webhooks/5ce1a21a-7972-40ec-812a-408e7e7dd642"
         }
 
-        fetch("https://notifywave.com/webhooks/f41ff0fd-4475-499c-b086-82d6012bbf16", {
+        fetch(path, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
