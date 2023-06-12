64c9e6c50899bcc6d8523f0a967a68d337873961
Víctor Falcón <victoor89@gmail.com>
2023-06-12 16:42:57 +0200

    Send auth event on Google Oauth too

--- src/pages/ServiceConnection.tsx
+++ src/pages/ServiceConnection.tsx
@@ -1,25 +1,44 @@
 import {useNavigate, useParams, useSearchParams} from "react-router-dom"
-import {useEffect} from "react"
+import {useEffect, useState} from "react"
 import {fetchApi} from "../helpers/ApiFetcher"
 import {DeviceName} from "../storage/DeviceName"
 import CircleArrowsIcon from "../components/icons/CircleArrowsIcon"
 import {useAuthState} from "../storage/AuthToken"
+import {Tokens} from "../types/Token"
+import {useUserState} from "../storage/User"
 
 export default function ServiceConnection() {
     const {isAuth} = useAuthState()
+    const {user, refreshUser} = useUserState(isAuth)
     const {driver} = useParams()
     const [searchParams] = useSearchParams()
+    const [userCreated, setUserCreated] = useState(false)
+    const [tokens, setTokens] = useState<Tokens>()
     const navigate = useNavigate()
 
     useEffect(() => {
-        fetchApi(`/auth/${driver}/callback?${searchParams.toString()}&deviceName=${DeviceName.name()}`, {
+        if (tokens === undefined) return
+        localStorage.setItem("nw:auth", JSON.stringify(tokens))
+        refreshUser()
+
+        if (!userCreated || !user) return
+
+        const referrer = localStorage.getItem("nw:referrer") ?? document.referrer
+        fetch(`https://metricswave.com/webhooks/f3fcf7cc-416d-4ff9-bc12-3878e9127ff7?email=${user.email}&referrer=${referrer}&step=signup`)
+
+        window.location.href = "/welcome"
+    }, [userCreated, user, tokens])
+
+    useEffect(() => {
+        fetchApi<Tokens>(`/auth/${driver}/callback?${searchParams.toString()}&deviceName=${DeviceName.name()}`, {
             success: (data) => {
                 if (!isAuth) {
-                    localStorage.setItem("nw:auth", JSON.stringify(data.data))
-                    window.location.href = "/"
+                    setTokens(data.data)
+                    setUserCreated(true)
+                    return
                 }
 
-                navigate(`/services?connected=true&driver=${driver}`)
+                navigate(`/`)
             },
             error: (error) => {
                 navigate("/")
@@ -28,11 +47,11 @@ export default function ServiceConnection() {
                 navigate("/")
             },
         })
-    })
+    }, [])
 
     return (
-            <div className="h-screen w-full flex flex-col items-center justify-center space-y-6 animate-pulse">
-                <CircleArrowsIcon className="w-12 h-12 animate-spin"/>
-            </div>
+        <div className="h-screen w-full flex flex-col items-center justify-center space-y-6 animate-pulse">
+            <CircleArrowsIcon className="w-12 h-12 animate-spin"/>
+        </div>
     )
 }
--- src/storage/User.ts
+++ src/storage/User.ts
@@ -6,6 +6,10 @@ import {DAY_SECONDS, expirableLocalStorage} from "../helpers/ExpirableLocalStora
 const USER_REFRESH_KEY: string = "nw:user:refresh"
 const USER_KEY: string = "nw:user"
 
+export const getUser = (): User | null => {
+    return expirableLocalStorage.get(USER_KEY, false)
+}
+
 export function useUserState(isAuth: boolean) {
     const [expired, setExpired] = useState(false)
 
@@ -13,27 +17,29 @@ export function useUserState(isAuth: boolean) {
         expirableLocalStorage.get(USER_REFRESH_KEY, false),
     )
 
-    const [user, setUser] = useState<User | null>(
-        expirableLocalStorage.get(USER_KEY, null),
-    )
+    const [user, setUser] = useState<User | null>(getUser())
+
+    const refreshUser = () => {
+        console.log("refreshUser")
+        fetchAuthApi<User>("/users", {
+            success: (data) => {
+                expirableLocalStorage.set(USER_REFRESH_KEY, true, DAY_SECONDS)
+                expirableLocalStorage.set(USER_KEY, data.data)
+                setUser(data.data)
+                setExpired(false)
+            },
+            error: (data) => setExpired(true),
+            catcher: (err) => setExpired(true),
+        })
+    }
 
     useEffect(() => {
         if (user !== null && isFreshUser) {
             return
         }
 
-        fetchAuthApi<User>("/users", {
-            success: (data) => {
-                expirableLocalStorage.set(USER_REFRESH_KEY, true, DAY_SECONDS)
-                expirableLocalStorage.set(USER_KEY, data.data)
-                setUser(data.data)
-                setIsFreshUser(true)
-                setExpired(false)
-            },
-            error: (data) => setExpired(true),
-            catcher: (err) => setExpired(true),
-        })
+        refreshUser()
     }, [isAuth])
 
-    return {user, setUser, expired, setExpired}
+    return {user, setUser, refreshUser, expired, setExpired}
 }
