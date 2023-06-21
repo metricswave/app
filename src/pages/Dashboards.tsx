dd727cd89ad00f0dfc9801565b1844a32091f986
Víctor Falcón <victoor89@gmail.com>
2023-06-21 10:39:33 +0200

    🐛 Bug: dashboard keep loading forever if dashboard has no items

--- src/pages/Dashboards.tsx
+++ src/pages/Dashboards.tsx
@@ -32,7 +32,7 @@ export function Dashboards() {
         setPeriod(period)
     }
     let addButtonSize = "w-full"
-    const dashboardsHasLoad = dashboards !== undefined && dashboards.length > 0 && dashboards[dashboardIndex] !== undefined && dashboards[dashboardIndex].items.length > 0
+    const dashboardsHasLoad = dashboards !== undefined && dashboards.length > 0 && dashboards[dashboardIndex] !== undefined
     if (dashboardsHasLoad) {
         addButtonSize = dashboards[dashboardIndex].items.length % 2 === 0 ? "" : "md:col-span-2"
     }
