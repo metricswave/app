af131b85c160b8d9e36a35deea739f224c5eed68
Víctor Falcón <victoor89@gmail.com>
2023-06-23 08:44:09 +0200

    Add Sentry to React App

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css"
 import reportWebVitals from "./reportWebVitals"
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import {routes} from "./routes/routes"
+import * as Sentry from "@sentry/react"
 
 const router = createBrowserRouter(routes)
 
@@ -19,6 +20,22 @@ if (urlRef !== null) {
 ) {
     localStorage.setItem("metricswave:referrer", document.referrer)
 }
+
+Sentry.init({
+    dsn: "https://44dd33b9ab074fd8bbae88f64b7bce87@o4505407458902016.ingest.sentry.io/4505407464079360",
+    integrations: [
+        new Sentry.BrowserTracing({
+            // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+            tracePropagationTargets: [/localhost/, /^https:\/\/metricswave\.com/],
+        }),
+        new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 0.5, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+})
 
 root.render(
     // <React.StrictMode>
