64443e96ab475c0074a2dee5d63d2b445c875686
Víctor Falcón <victoor89@gmail.com>
2023-06-23 12:23:40 +0200

    Track Vistis using script instead of custom implementation

--- src/storage/VisitTracker.ts
+++ src/storage/VisitTracker.ts
@@ -1,31 +1,29 @@
 import {useLocation} from "react-router-dom"
-import {useEffect} from "react"
-import {app} from "../config/app"
 
 export const TrackVisit = function () {
     const location = useLocation()
     const searchParams = new URLSearchParams(location.search)
 
-    useEffect(() => {
-        const params = {
-            path: location.pathname,
-            referrer: searchParams.get("utm_source") ?? document.referrer,
-        }
-
-        if (!app.isProduction) {
-            console.log("VisitTracker", params)
-            return
-        }
-
-        const path = "https://notifywave.com/webhooks/f41ff0fd-4475-499c-b086-82d6012bbf16"
-
-        fetch(path, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify(params),
-        })
-    }, [location])
+    // useEffect(() => {
+    //     const params = {
+    //         path: location.pathname,
+    //         referrer: searchParams.get("utm_source") ?? document.referrer,
+    //     }
+    //
+    //     if (!app.isProduction) {
+    //         console.log("VisitTracker", params)
+    //         return
+    //     }
+    //
+    //     const path = "https://notifywave.com/webhooks/f41ff0fd-4475-499c-b086-82d6012bbf16"
+    //
+    //     fetch(path, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             "Accept": "application/json",
+    //         },
+    //         body: JSON.stringify(params),
+    //     })
+    // }, [location])
 }
