7f5b8e9bd313a08fa22e30ca174e8214762e627f
Víctor Falcón <victoor89@gmail.com>
2023-06-30 07:42:30 +0200

    Reduce de number of code bundles

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,22 +5,22 @@ import {Dashboards} from "../pages/Dashboards"
 import {Welcome} from "../pages/Welcome"
 import {PublicDashboard} from "../pages/PublicDashboard"
 import LoadingPage from "../pages/LoadingPage"
+import Authentication from "../layouts/Authentication"
+import SignUp from "../pages/SignUp"
+import Login from "../pages/Login"
+import ForgotPassword from "../pages/ForgotPassword"
+import ResetPassword from "../pages/ResetPassword"
+import Services from "../pages/Services"
+import ErrorPage from "../pages/ErrorPage"
+import Notifications from "../pages/Notifications"
+import Settings from "../pages/Settings/Settings"
+import ProfileSettings from "../pages/Settings/ProfileSettings"
+import BillingSettings from "../pages/Settings/BillingSettings"
+import Triggers from "../pages/Triggers"
 
-const Services = lazy(() => import("../pages/Services"))
-const ErrorPage = lazy(() => import("../pages/ErrorPage"))
-const Triggers = lazy(() => import("../pages/Triggers"))
-const Settings = lazy(() => import("../pages/Settings/Settings"))
-const BillingSettings = lazy(() => import("../pages/Settings/BillingSettings"))
-const ProfileSettings = lazy(() => import("../pages/Settings/ProfileSettings"))
 const ServiceConnection = lazy(() => import("../pages/ServiceConnection"))
 const Trigger = lazy(() => import("../pages/Trigger"))
 const TriggerEdit = lazy(() => import("../pages/TriggerEdit"))
-const Notifications = lazy(() => import("../pages/Notifications"))
-const Authentication = lazy(() => import("../layouts/Authentication"))
-const SignUp = lazy(() => import("../pages/SignUp"))
-const Login = lazy(() => import("../pages/Login"))
-const ForgotPassword = lazy(() => import("../pages/ForgotPassword"))
-const ResetPassword = lazy(() => import("../pages/ResetPassword"))
 
 export const routes: RouteObject[] = [
     {
@@ -70,7 +70,7 @@ export const routes: RouteObject[] = [
     },
     {
         path: "/auth/:driver/callback",
-        element: <ServiceConnection/>,
+        element: <Suspense fallback={<LoadingPage/>}><ServiceConnection/></Suspense>,
     },
     {
         path: "/welcome",
