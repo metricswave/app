92b59cdd734a7245972dccb99072ba8cd343d2d7
Víctor Falcón <victoor89@gmail.com>
2024-09-23 14:38:53 +0200

    Change price to EUR by default

--- src/helpers/PriceFormatter.ts
+++ src/helpers/PriceFormatter.ts
@@ -1,4 +1,4 @@
 export function price_formatter(n: number): string {
-    const formatter = new Intl.NumberFormat("en-US", {"style": "currency", "currency": "USD"})
+    const formatter = new Intl.NumberFormat("en-US", {"style": "currency", "currency": "EUR"})
     return formatter.format(n / 100)
 }
