99a6dd58ed8f64303fdc14d7c8b1a5e338c7af8b
Víctor Falcón <victoor89@gmail.com>
2024-09-25 16:57:58 +0200

    Avoid to send the same event twice

--- src/helpers/EventTracker.ts
+++ src/helpers/EventTracker.ts
@@ -1,29 +1,48 @@
 import {app} from "../config/app"
 import ReactPixel from 'react-facebook-pixel';
 
+const eventLastExecuted: { [key: string]: number } = {}
+const eventUuidLastExecuted: { [key: string]: number } = {}
+
 export default {
     track: (eventUuid: string, params: Object = {}) => {
-        if (!app.isProduction) {
-            console.log(`[EventTracker] ${eventUuid}`, params)
+        const now = Date.now();
+        const lastExecutedTime = eventUuidLastExecuted[eventUuid];
+
+        if (lastExecutedTime !== undefined && now - lastExecutedTime < 3000) {
             return
         }
 
-        fetch(`https://metricswave.com/webhooks/${eventUuid}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            },
-            body: JSON.stringify(params),
-        })
+        if (!app.isProduction) {
+            console.log(`[EventTracker] ${eventUuid}`, params)
+        } else {
+            fetch(`https://metricswave.com/webhooks/${eventUuid}`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json",
+                },
+                body: JSON.stringify(params),
+            })
+        }
+
+        eventUuidLastExecuted[eventUuid] = now
     },
 
     pixelEvent: (event: string, params: Object = {}) => {
-        if (!app.isProduction) {
-            console.log(`[EventTracker] ${event}`, params)
+        const now = Date.now();
+        const lastExecutedTime = eventLastExecuted[event];
+
+        if (lastExecutedTime !== undefined && now - lastExecutedTime < 3000) {
             return
         }
 
-        ReactPixel.track(event, params)
+        if (!app.isProduction) {
+            console.log(`[EventTracker] ${event}`, params)
+        } else {
+            ReactPixel.track(event, params)
+        }
+
+        eventLastExecuted[event] = now
     },
 }
