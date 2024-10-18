cd64cc2717284d35af117c7f15760923606b5087
Víctor Falcón <victoor89@gmail.com>
2024-10-18 15:16:48 +0200

    Wrong redirection

--- src/helpers/PortalCheckout.ts
+++ src/helpers/PortalCheckout.ts
@@ -1,11 +1,12 @@
 import {fetchAuthApi} from "./ApiFetcher"
 
 export function portalCheckout(teamId: number, redirectTo: string) {
-    fetchAuthApi<{ url: string }>(
+    fetchAuthApi<{ path: string }>(
         `/checkout/${teamId}/portal-path?redirect-to=${redirectTo}`,
         {
             success: (data) => {
-                window.location.href = data.data.url
+                console.log(`Data`, data)
+                window.location.href = data.data.path
             },
             error: (err: any) => null,
             catcher: (err: any) => null,
