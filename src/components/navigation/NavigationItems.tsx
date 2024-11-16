11f6afec2e47795768599011a6facd4fe8dee3e8
Víctor Falcón <victoor89@gmail.com>
2024-11-16 18:15:35 +0000

    Change desktop navmenu to have 4 options

--- src/components/navigation/NavigationItems.tsx
+++ src/components/navigation/NavigationItems.tsx
@@ -1,26 +1,30 @@
-import React from "react"
-import {items} from "./Items"
-import {isCurrentRoute} from "../../helpers/Routes"
+import React from "react";
+import { items } from "./Items";
+import { isCurrentRoute } from "../../helpers/Routes";
 
 export default function NavigationItems() {
     return (
-            <ul className={"flex flex-row space-x-4"}>
-                {items.map(({icon, label, path}, index) => {
-                    const current = isCurrentRoute(window.location.pathname, path)
+        <ul className={"flex flex-row items-center space-x-2"}>
+            {items.map(({ icon, label, path }, index) => {
+                const current = isCurrentRoute(window.location.pathname, path);
 
-                    return (
-                            <li key={`${index}_item`}>
-                                <a href={path}
-                                   className={[
-                                       "flex flex-row items-center justify-center smooth hover:bg-[var(--menu-item-hover)] rounded-full px-4 py-2",
-                                       (current ? "bg-[var(--menu-item-active)]" : ""),
-                                   ].join(" ")}>
-                                    {React.createElement(icon, {className: "text-base mr-2"})}
-                                    <span className="">{label}</span>
-                                </a>
-                            </li>
-                    )
-                })}
-            </ul>
-    )
+                return (
+                    <li key={`${index}_item`}>
+                        <a
+                            href={path}
+                            className={[
+                                "flex flex-row items-center justify-center smooth hover:bg-[var(--menu-item-hover)] rounded-full px-4 py-2",
+                                current ? "bg-[var(--menu-item-active)]" : "",
+                            ].join(" ")}
+                        >
+                            {React.createElement(icon, { className: "text-base" })}
+                            <span className={["ml-2", !current ? "hidden lg:inline-block" : ""].join(" ")}>
+                                {label}
+                            </span>
+                        </a>
+                    </li>
+                );
+            })}
+        </ul>
+    );
 }
