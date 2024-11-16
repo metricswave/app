88ec5707431cd1b607eda60d937edbf2c08b6007
Víctor Falcón <victoor89@gmail.com>
2024-11-16 18:56:08 +0000

    Format Realtime notifications

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -8,44 +8,72 @@ import SectionContainer from "../components/sections/SectionContainer";
 export default function HistoryPage() {
     const { notifications } = useNotificationsStage();
 
+    const formatTitleDate = function (date: string): string {
+        const dt = DateJs.from(date);
+        return dt.toLocaleString({ month: "long", day: "numeric", weekday: "short" });
+    };
+
+    const aDayBefore = function (before: string, current: string): boolean {
+        const bdt = DateJs.from(before);
+        const cdt = DateJs.from(current);
+
+        return bdt.ordinal < cdt.ordinal;
+    };
+
     if (notifications.length === 0) return <NotificationsPageEmptyState />;
 
     return (
         <SectionContainer>
-            <div className="flex flex-col space-y-6 max-w-content">
+            <div className="flex flex-col space-y-4 max-w-content">
                 <PageTitle title="Realtime notifications" />
 
-                <div className="flex flex-col space-y-4 rounded-sm p-4 border soft-border text-sm sm:text-base animate-pulse bg-amber-50/50 dark:bg-zinc-800/50">
+                <div className="flex flex-col space-y-4 rounded-sm p-3 border soft-border text-sm animate-pulse bg-amber-50/50 dark:bg-zinc-800/50">
                     Loading …
                 </div>
 
-                {notifications.map((notification) => {
+                {notifications.map((notification, key) => {
+                    let showDate = false;
+                    const previous = notifications[key - 1] ?? null;
+
+                    if (previous === null) {
+                        showDate = true;
+                    } else {
+                        showDate = aDayBefore(previous.created_at, notification.created_at);
+                    }
+
                     return (
-                        <div
-                            key={notification.id}
-                            className="flex flex-col space-y-4 rounded-sm p-4 border soft-border text-sm sm:text-base"
-                        >
-                            <div className="flex flex-row items-center justify-start space-x-4">
-                                <div className="text-3xl w-11 h-11 bg-pink-500/10 flex items-center justify-center rounded border border-pink-500/25">
-                                    <span>{notification.data.emoji}</span>
+                        <>
+                            {showDate && (
+                                <h3 className="pt-4 opacity-75 uppercase text-sm">
+                                    {formatTitleDate(notification.created_at)}
+                                </h3>
+                            )}
+
+                            <div
+                                key={notification.id}
+                                className="flex flex-col space-y-2 rounded-sm p-3 border soft-border text-sm sm:text-base"
+                            >
+                                <div className="flex flex-row items-start justify-start">
+                                    <div className="flex flex-row w-full items-center justify-between space-y-1">
+                                        <h2 className="flex flex-row items-center gap-3 font-bold text-sm">
+                                            <span className="text-lg">{notification.data.emoji}</span>
+                                            <span>{notification.data.title}</span>
+                                        </h2>
+
+                                        <p
+                                            title={DateJs.toRfc(notification.created_at)}
+                                            className="text-xs opacity-70 cursor-help"
+                                        >
+                                            {DateJs.relative(notification.created_at)}
+                                        </p>
+                                    </div>
                                 </div>
 
-                                <div className="flex flex-col items-start space-y-1">
-                                    <h2 className="font-bold">{notification.data.title}</h2>
-
-                                    <p
-                                        title={DateJs.toRfc(notification.created_at)}
-                                        className="text-sm opacity-70 cursor-help"
-                                    >
-                                        {DateJs.relative(notification.created_at)}
-                                    </p>
+                                <div className="prose-sm prose dark:prose-invert prose-p:my-0.5">
+                                    <ReactMarkdown>{notification.data.content.replaceAll("\n", "\n\n")}</ReactMarkdown>
                                 </div>
                             </div>
-
-                            <div className="prose dark:prose-invert prose-p:my-0.5">
-                                <ReactMarkdown>{notification.data.content.replaceAll("\n", "\n\n")}</ReactMarkdown>
-                            </div>
-                        </div>
+                        </>
                     );
                 })}
             </div>
