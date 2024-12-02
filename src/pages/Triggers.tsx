b280d3bd4d4787697cc3296e5008f14c0eb6c98e
Víctor Falcón <victoor89@gmail.com>
2024-12-02 16:22:06 +0100

    feat: Update Triggers component with auth context and improved tracking

--- src/pages/Triggers.tsx
+++ src/pages/Triggers.tsx
@@ -1,63 +1,87 @@
-import React from "react"
-import PageTitle from "../components/sections/PageTitle"
-import SectionContainer from "../components/sections/SectionContainer"
-import {DialogComponent} from "../components/dialog/DialogComponent"
-import TriggersAdd from "../components/triggers_add/TriggersAdd"
-import {useTriggersState} from "../storage/Triggers"
-import {NoLinkButton} from "../components/buttons/LinkButton"
-import TriggerBlock from "../components/triggers/TriggerBlock"
-import {useNavigate} from "react-router-dom"
-import {useTriggerTypesState} from "../storage/TriggerTypes"
-import eventTracker from "../helpers/EventTracker"
-import {useUserServicesState} from "../storage/UserServices"
-import TriggersPageEmptyState from "./TriggersPageEmptyState"
+import React from "react";
+import PageTitle from "../components/sections/PageTitle";
+import SectionContainer from "../components/sections/SectionContainer";
+import { DialogComponent } from "../components/dialog/DialogComponent";
+import TriggersAdd from "../components/triggers_add/TriggersAdd";
+import { useTriggersState } from "../storage/Triggers";
+import { NoLinkButton } from "../components/buttons/LinkButton";
+import TriggerBlock from "../components/triggers/TriggerBlock";
+import { useNavigate } from "react-router-dom";
+import { useTriggerTypesState } from "../storage/TriggerTypes";
+import eventTracker from "../helpers/EventTracker";
+import { useUserServicesState } from "../storage/UserServices";
+import TriggersPageEmptyState from "./TriggersPageEmptyState";
+import { useAuthContext } from "../contexts/AuthContext";
 
 export default function Triggers() {
-    useUserServicesState()
-    const navigate = useNavigate()
-    const {triggers, refreshTriggers} = useTriggersState()
-    const {getTriggerTypeById} = useTriggerTypesState()
+    useUserServicesState();
+    const { userState } = useAuthContext();
+    const navigate = useNavigate();
+    const { triggers, refreshTriggers } = useTriggersState();
+    const { getTriggerTypeById } = useTriggerTypesState();
 
     if (triggers.length === 0) {
-        return <TriggersPageEmptyState>
-            {/* Add triggers dialog */}
-            <DialogComponent onOpenChange={status => {
-                if (status) {
-                    eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {step: 1})
-                }
-            }}
-                             button={
-                                 <div className="border soft-border rounded-sm p-4 flex flex-col space-y-4 items-center hover:bg-[var(--background-50-color)] smooth cursor-pointer">
-                                     <NoLinkButton text="Add trigger"/>
-                                 </div>
-                             }>
-                <TriggersAdd onLastStep={() => {
-                    refreshTriggers()
-                    eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {step: 2})
-                }}/>
-            </DialogComponent>
-        </TriggersPageEmptyState>
+        return (
+            <TriggersPageEmptyState>
+                {/* Add triggers dialog */}
+                <DialogComponent
+                    onOpenChange={(status) => {
+                        if (status) {
+                            eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {
+                                step: 1,
+                                user: userState.user?.email,
+                            });
+                        }
+                    }}
+                    button={
+                        <div className="border soft-border rounded-sm p-4 flex flex-col space-y-4 items-center hover:bg-[var(--background-50-color)] smooth cursor-pointer">
+                            <NoLinkButton text="Add trigger" />
+                        </div>
+                    }
+                >
+                    <TriggersAdd
+                        onLastStep={() => {
+                            refreshTriggers();
+                            eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {
+                                step: 2,
+                                user: userState.user?.email,
+                            });
+                        }}
+                    />
+                </DialogComponent>
+            </TriggersPageEmptyState>
+        );
     }
 
     return (
         <SectionContainer>
-            <PageTitle title="Events"/>
+            <PageTitle title="Events" />
 
             {/* Add triggers dialog */}
-            <DialogComponent onOpenChange={status => {
-                if (status) {
-                    eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {step: 1})
+            <DialogComponent
+                onOpenChange={(status) => {
+                    if (status) {
+                        eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {
+                            step: 1,
+                            user: userState.user?.email,
+                        });
+                    }
+                }}
+                button={
+                    <div className="border soft-border rounded-sm p-4 flex flex-col space-y-4 items-center hover:bg-[var(--background-50-color)] smooth cursor-pointer">
+                        <NoLinkButton text="Add Event" />
+                    </div>
                 }
-            }}
-                             button={
-                                 <div className="border soft-border rounded-sm p-4 flex flex-col space-y-4 items-center hover:bg-[var(--background-50-color)] smooth cursor-pointer">
-                                     <NoLinkButton text="Add Event"/>
-                                 </div>
-                             }>
-                <TriggersAdd onLastStep={() => {
-                    eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {step: 2})
-                    refreshTriggers()
-                }}/>
+            >
+                <TriggersAdd
+                    onLastStep={() => {
+                        eventTracker.track("0555d93b-350a-41b4-800c-8741b9566bb0", {
+                            step: 2,
+                            user: userState.user?.email,
+                        });
+                        refreshTriggers();
+                    }}
+                />
             </DialogComponent>
 
             <div className="flex flex-col space-y-6">
@@ -67,12 +91,11 @@ export default function Triggers() {
                         triggerType={getTriggerTypeById(trigger.trigger_type_id)}
                         key={trigger.id}
                         onClick={() => {
-                            navigate(`/events/${trigger.uuid}`)
+                            navigate(`/events/${trigger.uuid}`);
                         }}
                     />
                 ))}
             </div>
-
         </SectionContainer>
-    )
+    );
 }
